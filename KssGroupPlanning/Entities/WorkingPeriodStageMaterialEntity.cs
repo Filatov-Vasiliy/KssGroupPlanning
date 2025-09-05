44d58a63991601804be1132a3c69e0666c48a4df
unknown <filatov9000@mail.ru>
2025-09-05 13:37:40 +0300

    Добавлены две сущности Бригада и Материалы для ВоркингПериода

---
+++ KssGroupPlanning/Entities/WorkingPeriodStageMaterialEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace KssGroupPlanning.Entities
+{
+    public class WorkingPeriodStageMaterialEntity
+    {
+        public Guid Id { get; set; }
+        public WorkingPeriodStageEntity WorkingPeriodStageEntity { get; set; }
+        public Guid WorkingPeriodStageId { get; set; }
+        public MaterialSampleEntity MaterialSampleEntity { get; set; }
+        public Guid MaterialSampleId {get; set; }
+        public DateOnly DateDelivery { get; set; }
+    }
+}
