d0ebb1d894be7631888ad3da06784177dead66e3
unknown <filatov9000@mail.ru>
2025-12-15 15:20:08 +0300

    Создание ядрышка

---
+++ KssGroupPlanning/Services/CoreService.cs
@@ -0,0 +1,18 @@
+﻿using KssGroupPlanning.Interfaces.Repository;
+using KssGroupPlanning.Models;
+
+namespace KssGroupPlanning.Services
+{
+    public class CoreService
+    {
+        private readonly IProductTypeRepository _productTypeRepository;
+        public CoreService(IProductTypeRepository productTypeRepository)
+        {
+            _productTypeRepository = productTypeRepository;
+        }
+        public async Task<List<ProductType>> GetAll()
+        {
+            return await _productTypeRepository.GetAll();
+        }
+    }
+}
