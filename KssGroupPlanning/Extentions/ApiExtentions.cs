9b4d815f8782534024e0ca7f9046c67dc294ddac
Дмитрий Тельнов <vodinoj001@mail.ru>
2026-01-22 14:02:26 +0300

    Добавил ендпоинт для WorkingPeriodStageBrigadeRelation

--- KssGroupPlanning/Extentions/ApiExtentions.cs
+++ KssGroupPlanning/Extentions/ApiExtentions.cs
@@ -24,6 +24,7 @@ public static class ApiExtentions
         app.MapWorkingPeriodStageMaterialEndpoints();
 
         app.MapCoreEndpoints(); //?!?!?!
+        app.MapWorkingPeriodStageBrigadeRelationEndpoints();
 
     }
 }
