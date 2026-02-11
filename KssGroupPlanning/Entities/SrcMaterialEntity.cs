8203073e2d11c489c2d68b239c71df970d1f9ed7
unknown <filatov9000@mail.ru>
2026-02-11 13:12:35 +0300

    Добавлен FK  product -> stage субизделие

--- KssGroupPlanning/Entities/SrcMaterialEntity.cs
+++ KssGroupPlanning/Entities/SrcMaterialEntity.cs
@@ -8,6 +8,7 @@ namespace KssGroupPlanning.Entities
         [Name("Product order name")]
         public string ProductOrderName { get; set; }
         [Name("Product_order_date")]
+        [Format("dd.mm.yyyy")]
         public DateOnly ProductOrderDate { get; set; }
         [Name("Material Name")]
         public string MaterialName { get; set; }
@@ -18,12 +19,18 @@ namespace KssGroupPlanning.Entities
         [Name("Current qty")]
         public decimal CurrentQty { get; set; }
         [Name("Posted Date")]
+        [Format("dd.mm.yyyy")]
         public DateOnly PostedDate { get; set; }
         [Name("For admission date")]
+        [Format("dd.mm.yyyy")]
         public DateOnly ForAdmissionDate { get; set; }
         [Name("Create Date")]
+        [Format("dd.mm.yyyy")]
         public DateOnly CreateDate { get; set; }
         [Name("Product order name child")]
         public string ProductOrderNameChild { get; set; }
+        [Name("Product order name child date")]
+        [Format("dd.mm.yyyy")]
+        public string ProductOrderNameChildDate { get; set; }
     }
 }
